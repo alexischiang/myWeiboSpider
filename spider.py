0a7860056513c33abf7c323635aa5f01aced8d49
whitelord <1091285927@qq.com>
2019-06-12 09:15:55 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -6,6 +6,7 @@ from lxml import etree
 
 class weibo:
     def __init__(self, user_id, cookies):
+        """初始化"""
         self.user_id = user_id
         self.cookies = {
             'Cookie' : cookies
@@ -15,10 +16,13 @@ class weibo:
         self.area = []
         self.birthday = []
         self.intro = []
+        self.total_weibo = []
+        self.following = []
+        self.follower = []
 
-    def get_html(self):
+    def get_html(self,url):
+        """获取传入url的html文本"""
         try:
-            url = 'https://weibo.cn/' + self.user_id + '/info'
             html = requests.get(url= url, cookies= self.cookies).content
             selector = etree.HTML(html)
             return selector
@@ -27,21 +31,47 @@ class weibo:
             traceback.print_exc()
 
     def get_userinfo(self):
-        selector = self.get_html()
+        """获取昵称、性别等信息"""
+        # 读取html
+        url = 'https://weibo.cn/' + self.user_id + '/info'
+        selector = self.get_html(url)
+        # 筛选html
         info_list = selector.xpath('/html/body/div[8]/text()')
+        # 存储
         self.name.append(info_list[0][1:])
         self.sex.append(info_list[1][1:])
         self.area.append(info_list[2][1:])
         self.birthday.append(info_list[3][1:])
         self.intro.append(info_list[5][1:])
 
+    def get_userinfo2(self):
+        """获取微博条数、关注数、粉丝数"""
+        # 读取html
+        url = 'https://weibo.cn/' + self.user_id + '/profile'
+        selector = self.get_html(url)
+        # 筛选html + 存储
+        self.total_weibo.append(selector.xpath('/html/body/div[3]/div[2]/span/text()')[0])
+        self.following.append(selector.xpath('/html/body/div[3]/div[2]/a[1]/text()')[0])
+        self.follower.append(selector.xpath('/html/body/div[3]/div[2]/a[2]/text()')[0])
+
+        # 筛选 + 存储（法2）
+        # info_list = selector.xpath("//div[@class='tip2']/*/text()")
+        # self.total_weibo.append(info_list[0])
+        # self.following(info_list[1])
+        # self.follower(info_list[2])
+
+
     def show(self):
         self.get_userinfo()
+        self.get_userinfo2()
         print(self.name)
         print(self.sex)
         print(self.area)
         print(self.birthday)
         print(self.intro)
+        print(self.total_weibo)
+        print(self.following)
+        print(self.follower)
 
 if __name__ == "__main__":
     cookies = 'SCF=AmMdYdAD8xDP84Xc7sEtL9WXFMVx_fALJyadgeh6G41PUqyXV4VQ_9g8MWqBiH82U_5rDZFKsxg0w-CrGae8IXg.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFr0NVib_A0gaGkWL.D5ObN5JpX5K-hUgL.FozceK2R1K2cSKe2dJLoI0qLxKqLBKBLBo5LxK-LB-BL1K5LxKqLBo2L1h2LxKqL1hnL1K2LxKML1hnLBo2LxK-L1KqL1-Bt; _T_WM=52050499844; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; MLOGIN=0; SUB=_2A25x-7enDeRhGeRI6lMZ-S_Kzj-IHXVTB9nvrDV6PUJbkdBeLVrMkW1NUtpT2XBkCAb9xERrHGGHTjLUkQZBtos0; SUHB=08J6kbAgs1djdm; SSOLoginState=1560266743'
