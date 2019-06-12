3520f7fcae13957b0b713ce8d999dc1af778afc3
whitelord <1091285927@qq.com>
2019-06-12 13:35:43 +0800

    -

--- spider.py
+++ spider.py
@@ -1,4 +1,5 @@
 import requests 
+from urllib.parse import urlencode
 
 # 异常处理库
 import traceback
@@ -63,7 +64,7 @@ class weibo:
         # self.following(info_list[1])
         # self.follower(info_list[2])
 
-    def get_total_page(self):
+    def get_total_page_num(self):
         """获取微博总页数"""
         # 读取html
         url = 'https://weibo.cn/' + self.user_id + '/profile'
@@ -71,16 +72,18 @@ class weibo:
         # 筛选html
         self.total_page = selector.xpath("/html/body/div[@class='pa']/form/div/text()")[1][-4:-1]
 
-    def get_one_page_ori(self):
-        """获取单页的原创微博"""
+    def get_one_page(self,page_num):
         # 读取html
         params = {
-            'filter' : '1',
-            'page' : '1'
+            'filter' : self.filter,
+            'page' : page_num
         }
-        url = 'https://weibo.cn/' + self.user_id + '/profile?'
+        base_url = 'https://weibo.cn/' + self.user_id + '/profile?'
+        url = base_url + urlencode(params)
         selector = self.get_html(url)
         # 筛选html
+        for index in range(2,11):
+            self.blog.append(selector.xpath("/html/body/div[@class='c'][" + str(index) + "]/div[1]/span[1]//text()"))
 
 
            
@@ -89,8 +92,8 @@ class weibo:
     def show(self):
         self.get_userinfo()
         self.get_userinfo2()
-        self.get_total_page()
-
+        self.get_total_page_num()
+        self.get_one_page(1)
 
         print(self.name)
         print(self.sex)
@@ -101,10 +104,13 @@ class weibo:
         print(self.following)
         print(self.follower)
         print(self.total_page)
+        for index in range(0,9):
+            print(self.blog[index])
+
 
 if __name__ == "__main__":
     cookies = 'SCF=AmMdYdAD8xDP84Xc7sEtL9WXFMVx_fALJyadgeh6G41PUqyXV4VQ_9g8MWqBiH82U_5rDZFKsxg0w-CrGae8IXg.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFr0NVib_A0gaGkWL.D5ObN5JpX5K-hUgL.FozceK2R1K2cSKe2dJLoI0qLxKqLBKBLBo5LxK-LB-BL1K5LxKqLBo2L1h2LxKqL1hnL1K2LxKML1hnLBo2LxK-L1KqL1-Bt; _T_WM=52050499844; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; MLOGIN=0; SUB=_2A25x-7enDeRhGeRI6lMZ-S_Kzj-IHXVTB9nvrDV6PUJbkdBeLVrMkW1NUtpT2XBkCAb9xERrHGGHTjLUkQZBtos0; SUHB=08J6kbAgs1djdm; SSOLoginState=1560266743'
-    weibo = weibo('2611891653',cookies)
+    weibo = weibo('2611891653',cookies,1)
     weibo.show()
 
 
