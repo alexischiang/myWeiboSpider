d5cd63bef2c0c676e6b06beaa6ae9df19510dcf2
whitelord <1091285927@qq.com>
2021-02-02 17:23:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
 }
 cookies = {
-    'Cookie' : 'SCF=AmMdYdAD8xDP84Xc7sEtL9WXFMVx_fALJyadgeh6G41PUqyXV4VQ_9g8MWqBiH82U_5rDZFKsxg0w-CrGae8IXg.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFr0NVib_A0gaGkWL.D5ObN5JpX5K-hUgL.FozceK2R1K2cSKe2dJLoI0qLxKqLBKBLBo5LxK-LB-BL1K5LxKqLBo2L1h2LxKqL1hnL1K2LxKML1hnLBo2LxK-L1KqL1-Bt; _T_WM=52050499844; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; MLOGIN=0; SUB=_2A25x-7enDeRhGeRI6lMZ-S_Kzj-IHXVTB9nvrDV6PUJbkdBeLVrMkW1NUtpT2XBkCAb9xERrHGGHTjLUkQZBtos0; SUHB=08J6kbAgs1djdm; SSOLoginState=1560266743'
+    'Cookie' : ''
 }
-html = requests.get('https://weibo.cn/2611891653/profile?filter=0&page=5', headers= headers, cookies= cookies).content
+html = requests.get('https://weibo.cn//profile?filter=0&page=5', headers= headers, cookies= cookies).content
 selector = etree.HTML(html)
 
 re_content_list = []
